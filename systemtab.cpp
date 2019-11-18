c86bbc2a4a8fe34527c46519885ac69824b676aa
Caleb Smith <karagenit@outlook.com>
2019-11-18 13:25:46 -0500

    Add Basic System Info Labels

--- systemtab.cpp
+++ systemtab.cpp
@@ -4,8 +4,17 @@
 
 SystemTab::SystemTab(QWidget *parent) : QWidget(parent)
 {
-    QLabel *label = new QLabel(tr("Tab Contents"));
+    QLabel *os_version = new QLabel("OS Version: Debian 10");
+    QLabel *kernel_version = new QLabel("Kernel Version: 1.0.0");
+    QLabel *memory = new QLabel("Memory: 16 GB");
+    QLabel *cpu = new QLabel("Processor: Intel i5-3320M, 2 Cores @ 2.60 GHz");
+    QLabel *disk = new QLabel("Free Disk Space: 182 GB");
+
     QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(label);
+    layout->addWidget(os_version);
+    layout->addWidget(kernel_version);
+    layout->addWidget(memory);
+    layout->addWidget(cpu);
+    layout->addWidget(disk);
     setLayout(layout);
 }
