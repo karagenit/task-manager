326599dc604883e4c05b6bce865ab60b68f90ca8
Caleb Smith <karagenit@outlook.com>
2019-11-18 13:58:38 -0500

    Read Real System Info

--- systemtab.cpp
+++ systemtab.cpp
@@ -4,11 +4,11 @@
 
 SystemTab::SystemTab(QWidget *parent) : QWidget(parent)
 {
-    QLabel *os_version = new QLabel("OS Version: Debian 10");
-    QLabel *kernel_version = new QLabel("Kernel Version: 1.0.0");
-    QLabel *memory = new QLabel("Memory: 16 GB");
-    QLabel *cpu = new QLabel("Processor: Intel i5-3320M, 2 Cores @ 2.60 GHz");
-    QLabel *disk = new QLabel("Free Disk Space: 182 GB");
+    QLabel *os_version = new QLabel(QString::fromStdString(get_os_version()));
+    QLabel *kernel_version = new QLabel(QString::fromStdString(get_kernel_version()));
+    QLabel *memory = new QLabel(QString::fromStdString(get_memory()));
+    QLabel *cpu = new QLabel(QString::fromStdString(get_cpu_version()));
+    QLabel *disk = new QLabel(QString::fromStdString(get_free_disk()));
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(os_version);
@@ -17,4 +17,37 @@ SystemTab::SystemTab(QWidget *parent) : QWidget(parent)
     layout->addWidget(cpu);
     layout->addWidget(disk);
     setLayout(layout);
+}
+
+std::string SystemTab::get_os_version() {
+    return "OS Version: " + popen_string("hostnamectl | grep \"Operating\" | awk -F ':' '{ print $2 }' | awk '{$1=$1;print}' | tr -d '\n'");
+}
+
+std::string SystemTab::get_kernel_version() {
+    return "Kernel Version: " + popen_string("uname -r | tr -d '\n'");
+}
+
+std::string SystemTab::get_memory() {
+    return "Total Memory: " + popen_string("awk '/MemTotal/{print $2 \" \" $3}' /proc/meminfo | tr -d '\n'");
+}
+
+std::string SystemTab::get_cpu_version() {
+    return "Processor: " + popen_string("grep 'name' /proc/cpuinfo | uniq | awk -F ':' '{print $2}' | awk '{$1=$1;print}' | tr -d '\n'");
+}
+
+std::string SystemTab::get_free_disk() {
+    return "Free Disk Space: " + popen_string("df -h --output=avail --total | awk 'END {print $1}' | tr -d '\n'");
+}
+
+std::string SystemTab::popen_string(std::string cmd) {
+  std::string answer;
+  FILE *fp = popen(cmd.c_str(), "r");
+  char buffer[1024];
+  while (!feof(fp)) {
+    if (fgets(buffer, 1024, fp)) {
+      answer.append(buffer);
+    }
+  }
+  pclose(fp);
+  return answer;
 }
--- systemtab.h
+++ systemtab.h
@@ -8,6 +8,14 @@ class SystemTab : public QWidget
     Q_OBJECT
 public:
     explicit SystemTab(QWidget *parent = nullptr);
+    //TODO: implement this as part of some utils class? Doesn't really belong here,
+    //      and is duplicated in RunningProcess
+    std::string popen_string(std::string);
+    std::string get_os_version();
+    std::string get_kernel_version();
+    std::string get_memory();
+    std::string get_cpu_version();
+    std::string get_free_disk();
 
 signals:
 
